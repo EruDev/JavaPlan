761231d2105394faf6590a5d2faef6fef07b39dd
erudev <1027926875@qq.com>
2023-11-07 22:17:07 +0800

    CountDownLatch

---
+++ src/main/java/io/erudev/javaconcurrency/features/countdownlatch/CountDownLatchCase.java
@@ -0,0 +1,30 @@
+package io.erudev.javaconcurrency.features.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author pengfei.zhao
+ * @date 2023/11/7 22:07
+ */
+public class CountDownLatchCase {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        CountDownLatch cdl = new CountDownLatch(2);
+        service.execute(() -> {
+            System.out.println("T1, " + Thread.currentThread().getName());
+            cdl.countDown();
+        });
+        service.execute(() -> {
+            System.out.println("T2, " + Thread.currentThread().getName());
+            cdl.countDown();
+        });
+
+        cdl.await(1, TimeUnit.SECONDS);
+        System.out.println("T3, " + Thread.currentThread().getName());
+        service.shutdown();
+    }
+}
